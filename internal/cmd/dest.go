ae55ce67497b40eb1188c0b1f1f6305230f7d9c9
hwipl <33433250+hwipl@users.noreply.github.com>
2020-08-26 15:01:59 +0200

    add internal/cmd/dest.go
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ internal/cmd/dest.go
@@ -0,0 +1,50 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+const (
+	dummyPort = 65535
+)
+
+// dest stores information about a forwarding destination
+type dest struct {
+	ip    net.IP
+	srcIP net.IP
+}
+
+// getSourceIP gets the source IP used for the forwarding destination
+func (d *dest) getSourceIP() {
+	// create dummy connection to retrieve local address
+	addr := fmt.Sprintf("%s:%d", d.ip, dummyPort)
+	conn, err := net.Dial("udp", addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	// get local address of connection
+	laddr := conn.LocalAddr().(*net.UDPAddr)
+	d.srcIP = laddr.IP
+}
+
+// newDest creates and returns a new dest
+func newDest(addr string) *dest {
+	var dest dest
+
+	// parse IP address
+	ip := net.ParseIP(addr).To4()
+	if ip == nil {
+		// invalid IP, stop here
+		return nil
+	}
+	dest.ip = ip
+
+	// get source IP
+	dest.getSourceIP()
+
+	return &dest
+}
